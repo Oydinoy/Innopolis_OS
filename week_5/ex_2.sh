2b68b5cd0a116d56c4a830d80a6cc21198f97738
Oydi <Oydinosha@mail.ru>
2018-09-19 17:07:53 -0700

    Ex_1, ex_2 done

--- week_5/ex_2.sh
+++ week_5/ex_2.sh
@@ -1,5 +1,12 @@
-echo "1 4 65 3 76 8" > toWork
-for int in $toWork
+#!/usr/bin/env bash
+
+echo "1 4 65 3 76 8\n" > toWork.txt
+
+while true
 do
-    echo "$int"
+    if `ln toWork.txt toWork.txt.lock`; then
+        toPrint = $(tail -n 1 toWork.txt.lock)
+        printf "$((toPrint + 1))\n" >> toWork.txt.lock
+        rm toWork.txt.lock
+    fi
 done
