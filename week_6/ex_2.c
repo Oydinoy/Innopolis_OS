fb28f2cf4311bd45891213a1b47acd45bd2027c7
Oydi <Oydinosha@mail.ru>
2018-09-26 21:02:58 -0700

    + 3

--- week_6/ex_2.c
+++ week_6/ex_2.c
@@ -1,4 +1,29 @@
-//
-// Created by ADMIN on 9/26/2018.
-//
+#include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <memory.h>
 
+int main() {
+    char first[] = "Hello";
+    char second[64];
+    int n1 = (int) strlen(first);
+
+    //creating the pipe
+    int fd[2];
+    pipe(fd);
+
+    //changed part
+
+    pid_t child = fork();
+    if (child){
+        write(fd[1], first, n1 + 1);
+        printf("What is in the parent: %s\n", first);
+        close(fd[0]);
+    } else {
+        read(fd[0], second, sizeof(second));
+        printf("What is in the child: %s\n", second);
+        close(fd[1]);
+    }
+    return 0;
+}
